b5f8c9d0179bf104263ebe5c686046f41e773ebf
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-07-11 12:31:32 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Ronaiza Cardoso on 11/07/2020.
+//  Copyright © 2020 Ronaiza Cardoso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
