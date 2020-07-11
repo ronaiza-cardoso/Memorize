c7d89f6edb3d3c16f947b5e4de1576a46203987b
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-07-11 20:35:24 +0100

    feat: add observers into the modelview and the view

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -11,8 +11,23 @@ import Foundation
 struct MemoryGame<CardContent > {
     var cards: Array<Card>
     
-    func choose(card: Card) {
+    // all functions that modify the "self" must be marked as __mutating__
+    mutating func choose(card: Card) {
         print("card choosen: \(card)")
+        let choosenIndex: Int = self.index(of: card)
+        self.cards[choosenIndex].isFaceUp = !self.cards[choosenIndex].isFaceUp
+    }
+    
+    // of - public name
+    // card - private name
+    func index(of card: Card) -> Int {
+        for index in 0..<self.cards.count {
+            if self.cards[index].id == card.id {
+                return index
+            }
+        }
+        
+        return -1 // TODO: trow error
     }
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
